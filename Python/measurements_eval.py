68a4fa41725981ab164ef8b7538a53371b7403d6
5tr0bl <m.strobl@campus.tu-berlin.de>
2023-11-22 23:01:24 +0100

    divide measurements by related input signals

--- Python/measurements_eval.py
+++ Python/measurements_eval.py
@@ -1,14 +1,47 @@
 #import numpy as np
-import numpy as np
+#import numpy as np
 import os
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 import pyfar as pf
 
+def find_input_reference(wav_file_path, path_references):
+    wav_prefix = os.path.basename(wav_file_path).split('_')[0]
+    for ref_filename in os.listdir(path_references):
+        ref_prefix = ref_filename.split('_')[0]
+        if ref_prefix == wav_prefix:
+            print(f"Reference input file: {ref_filename}")
+            return ref_filename
+    # FileNotFoundError
+    print(f"No input reference found for {wav_file_path}")
+    return None
+        
 def process_and_plot_wav(wav_file_path, plot_path):
-    # Load the audio file using pyfar
-    signal = pf.io.read_audio(wav_file_path)
+    path_references = "Python\ReferenceInputs"
     file_path = os.path.splitext(os.path.basename(wav_file_path))[0]
+    print(f"Processing file: {file_path}")
 
+    # Get signals as pyfar Signals
+    output_signal = pf.io.read_audio(wav_file_path)
+    input_reference_path = os.path.join(path_references, 
+                                        find_input_reference(wav_file_path=file_path,
+                                                             path_references=path_references))
+    input_signal = pf.io.read_audio(input_reference_path)
+
+    # Pad zeros to the shorter signal to make both signals have the same length
+    max_length = max(output_signal.n_samples, input_signal.n_samples)
+    if output_signal.n_samples < max_length:
+        output_signal = pf.dsp.pad_zeros(output_signal, max_length - output_signal.n_samples)
+    if input_signal.n_samples < max_length:
+        input_signal = pf.dsp.pad_zeros(input_signal, max_length - input_signal.n_samples)
+    print(f"Length Output: {output_signal.n_samples} Samples.")
+    print(f"Length Input: {input_signal.n_samples} Samples.")
+
+    # divide output/input to obtain the transfer function
+    transfer_function = output_signal / input_signal # regularize/invert H here?
+
+    pf.plot.two_d.freq_group_delay_2d(transfer_function) # how to plot as graph in pyfar?
+
+    '''
     frequencies = signal.frequencies
     fft = np.fft.fft(signal.time)
     magnitude_spectrum = np.abs(fft)#signal.freq_raw.T 
@@ -25,6 +58,7 @@ def process_and_plot_wav(wav_file_path, plot_path):
     fig_phase.add_trace(go.Scatter(x=frequencies, y=phase.real, mode='lines'))
     fig_phase.update_layout(title=f'Phase Information - {os.path.basename(wav_file_path)}')
     fig_phase.write_html(os.path.join(plot_path, f'phase_information_{file_path}.html'))
+    '''
 
 path_linear_measurements = "Python\Measurements\Linear"
 path_thiran_measurements = "Python\Measurements\Thiran"
